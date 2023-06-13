288224e9b952477a7350e014d8f6dd680e03ebf1
Amir Masu <115700409+Amirmasu07@users.noreply.github.com>
2023-06-13 20:41:06 +0530

    Add files via upload

---
+++ src/ClassCompo/07LoaderCompo.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class LoaderCompo extends Component {
+    render() {
+        return (
+            <>
+                <div className='container border-top broder-bottom mt-2'>
+                    <img src="https://miro.medium.com/v2/resize:fit:1400/1*CsJ05WEGfunYMLGfsT2sXA.gif" alt="" width="400px" />
+                </div>
+
+            </>
+        )
+    }
+}
+
+export default LoaderCompo;
